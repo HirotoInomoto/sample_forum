e52b399b30f4bc9f5386581bef4d136361c2e398
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2025-07-05 19:37:56 +0900

    クエリ最適化 1 - 演習終了

--- forum/models.py
+++ forum/models.py
@@ -13,3 +13,18 @@ class Message(models.Model):
 
     def __str__(self):
         return self.content
+
+class Comment(models.Model):
+    content = models.CharField("内容", max_length=200)
+    created_at = models.DateTimeField("投稿日時", auto_now_add=True)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="comment")
+
+    def __str__(self):
+        return self.content
+
+class Tag(models.Model):
+    name = models.CharField("タグ名", max_length=20)
+    message = models.ManyToManyField(Message, blank=True, related_name="tag")
+
+    def __str__(self):
+        return self.name
