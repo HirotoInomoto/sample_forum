87b86387a0e0d2dc59b44d6c4526781f825945b5
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2025-07-05 18:55:49 +0900

    first commit

---
+++ forum/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Topic, Message
+
+def index(request):
+    TOPIC_LIST = Topic.objects.all()
+    context = {
+        "topics": TOPIC_LIST
+    }
+    return render(request, "forum/index.html", )
+
+
+def forum(request, topic):
+    topic = Topic.objects.get(name=topic)
+    messages = Message.objects.filter(topic=topic).order_by("created_at")
+
+    try:
+        message = request.POST["message"]
+        Message.objects.create(
+            topic=topic,
+            content=message,
+        )
+    except:
+        pass
+
+    contxt = {
+        "messages": messages,
+        "topic": topic,
+    }
+    return render(request, "forum/forum.html", context)
