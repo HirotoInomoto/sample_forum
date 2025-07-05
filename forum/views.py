80eab2d633daa9dcc7709e832f4f078a7aa015a1
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2025-07-05 19:36:06 +0900

    クエリ最適化 1 - 演習終了

--- forum/views.py
+++ forum/views.py
@@ -13,17 +13,37 @@ def forum(request, topic):
     topic = Topic.objects.get(name=topic)
     messages = Message.objects.filter(topic=topic).order_by("created_at")
 
-    try:
-        message = request.POST["message"]
-        Message.objects.create(
-            topic=topic,
-            content=message,
-        )
-    except:
-        pass
+    if request.method == "POST":
+
+        if "message" in request.POST:
+            
+            # データの作成１
+            # これまで扱っていた
+            # モデルに対してcreateメソッドを使用してデータを作成するやり方
+            message = request.POST["message"]
+            Message.objects.create(
+                topic=topic,
+                content=message,
+            )
+
+        elif "comment" in request.POST:
+            
+            # データの作成２
+            # forms.pyを導入した場合のデータを作成するやり方
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+
+                message_id = request.POST["comment"]
+                message = Message.objects.get(id=message_id)
+
+                comment_form.instance.message = message
+                comment_form.save()
+
+    comment_form = CommentForm()
 
     context = {
         "messages": messages,
         "topic": topic,
+        "comment_form": comment_form,
     }
     return render(request, "forum/forum.html", context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include("forum.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
